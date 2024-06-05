f90ab902a21db180e2ae507cbe75c458a7fc1a59
Domin C <dominikcedro00@gmail.com>
2024-06-05 13:52:02 +0200

    UPDATE changed the tests layout ADDED stroop ,digit substition and trail making as separate tests in the users (embedded), not yet routed

--- models/evaluations.py
+++ models/evaluations.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Evaluation(BaseModel):
@@ -9,8 +10,32 @@ class Evaluation(BaseModel):
     type: str
     version: int
     score: int
-    date_taken: str
+    date_taken: str # TODO zmienic na datetime
     custom_field_1: Optional[str]
     custom_field_2: Optional[str]
 
 
+class Stroop(BaseModel):
+    stroop_id: int
+    version: int
+    datetime: datetime
+    mistake_count: int
+    total_score: int
+
+
+class DigitSubstitution(BaseModel):
+    digit_sub_id: int
+    version: int
+    datetime: datetime
+    time: int
+    mistake_count: int
+    total_score: int
+
+
+class TrailMaking(BaseModel):
+    stroop_id: int
+    version: int
+    datetime: datetime
+    time: int
+    mistake_count: int
+    total_score: int
--- models/users.py
+++ models/users.py
@@ -7,6 +7,8 @@ description: This module contains model for Users collection
 from pydantic import BaseModel
 from typing import List, Optional
 
+from models.evaluations import Stroop, DigitSubstitution, TrailMaking
+
 
 class User(BaseModel):
     user_id: int
@@ -14,5 +16,6 @@ class User(BaseModel):
     last_name: str
     version: int
     email: str
-    test_ids: Optional[List[int]] = []
-
+    stroop: Optional[List[Stroop]] = []
+    digit_substitution: Optional[List[DigitSubstitution]] = []
+    trail_making: Optional[List[TrailMaking]] = []
