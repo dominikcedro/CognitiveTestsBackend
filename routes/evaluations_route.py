be4b1965ef882c0502a32702e8e3a8858334889b
Domin C <dominikcedro00@gmail.com>
2024-06-07 15:02:33 +0200

    ADD test_evaluations to test endpoints for evaluations handling

--- routes/evaluations_route.py
+++ routes/evaluations_route.py
@@ -67,3 +67,40 @@ async def post_digitsubstitution(user_id: int):
     if user is None:
         raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
     return user.get("digit_substitution", [])
+
+#
+# @evaluations_router.get("/digitsubstitution/{user_id}")
+# async def post_digitsubstitution(user_id: int):
+#     """
+#     GET operation on DIGIT SUBSTITUTION evaluation for selected user to get all in list
+#     """
+#     user = collection_users.find_one({"user_id": user_id})
+#     if user is None:
+#         raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
+#     return user.get("digit_substitution", [])
+#
+#
+# @evaluations_router.get("/digitsubstitution/{user_id}")
+# async def post_digitsubstitution(user_id: int):
+#     """
+#     GET operation on DIGIT SUBSTITUTION evaluation for selected user to get all in list
+#     """
+#     user = collection_users.find_one({"user_id": user_id})
+#     if user is None:
+#         raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
+#     return user.get("digit_substitution", [])
+
+
+@evaluations_router.get("/evaluations/{user_id}")
+async def get_all_evaluations(user_id: int):
+    """
+    GET operation on ALL evaluations for selected user to get all in list
+    """
+    user = collection_users.find_one({"user_id": user_id})
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
+    stroop = user.get("stroop", [])
+    digit_sub = user.get("digit_substitution", [])
+    trail_make = user.get("trail_making", [])
+    result = stroop + digit_sub + trail_make
+    return result
