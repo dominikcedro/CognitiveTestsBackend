31bc96aa13870687d3b5786afb298b042c32a79e
Domin C <dominikcedro00@gmail.com>
2024-06-12 14:43:57 +0200

    trailmaking endpoint accepts JWT tokens to post evaluation

--- routes/evaluations_route.py
+++ routes/evaluations_route.py
@@ -5,7 +5,7 @@ license: BSD 3.0
 description: fastAPI routing for evaluations
 """
 from fastapi import APIRouter, Depends
-from models.evaluations import PostStroopRequest
+from models.evaluations import PostStroopRequest, PostTrailMakingRequest
 from models.users import User
 from database.database import collection_evaluations, collection_users, collection_counters, check_for_collection_counters_null
 from schema.schemas import evaluation_serial_list, user_serial_list
@@ -16,21 +16,6 @@ from routes.user_route import get_next_sequence_value
 
 
 evaluations_router = APIRouter()
-
-# ### Evaluations - > embedded in User# POST
-# @evaluations_router.post("/stroop/{user_id}")
-# async def post_stroop(user_id: int, stroop: Stroop):
-#     """
-#     POST operation on STROOP evaluation for selected user
-#     """
-#     user = collection_users.find_one({"user_id": user_id})
-#     if user is None:
-#         raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
-#     collection_users.update_one(
-#         {"user_id": user_id},
-#         {"$push": {"stroop": dict(stroop)}}
-#     )
-#     return {"message": "Stroop test posted successfully"}
 
 from security.security_config import get_current_user, TokenData
 
@@ -57,25 +42,28 @@ async def post_stroop(stroop_request: PostStroopRequest, current_user: TokenData
     )
     return {"message": "Stroop test posted successfully"}
 
-@evaluations_router.post("/trailmaking/{user_id}")
-async def post_trailmaking(user_id: int, trailmaking: TrailMaking):
+@evaluations_router.post("/trailmaking")
+async def post_trailmaking(trail_making_request: PostTrailMakingRequest, current_user: TokenData = Depends(get_current_user)):
     """
     POST operation on TRAIL MAKING evaluation for selected user
     """
-    check_for_collection_counters_null()
+    user_id = current_user.user_id
     user = collection_users.find_one({"user_id": user_id})
     if user is None:
         raise HTTPException(status_code=404, detail=f"User with ID {user_id} doesn't exist")
-
-
-    trail_making = dict(trailmaking)
-    print(trail_making)
-    trail_making["trail_making_id"] = get_next_sequence_value("trailmaking_id")
+    trail_making_id = get_next_sequence_value("trailmaking_id")
+    trail_making_in_db = { # TODO change to model using model from evaluations file
+        "stroop_id": trail_making_id,
+        "version": 1,  # TODO what do i need it for?
+        "datetime": trail_making_request.datetime,
+        "mistake_count": trail_making_request.mistake_count,
+        "total_score": trail_making_request.total_score
+    }
     collection_users.update_one(
         {"user_id": user_id},
-        {"$push": {"trail_making": trail_making}}
+        {"$push": {"trail_making": trail_making_in_db}}
     )
-    return {"message": "Trail Making test posted successfully"}
+    return {"message": f"Trail Making test posted successfully with id: {trail_making_in_db}"}
 
 @evaluations_router.post("/digitsubstitution/{user_id}")
 async def post_digitsubstitution(user_id: int, digitsubstitution: DigitSubstitution):
