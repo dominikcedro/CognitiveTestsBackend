e78cd134b2dd90f95aa6754e7ca187837c1e9eb3
Domin C <dominikcedro00@gmail.com>
2024-06-24 13:42:12 +0200

    UPDATE changed the stroop test verification

--- common/total_score_calculation.py
+++ common/total_score_calculation.py
@@ -5,22 +5,33 @@ license: BSD 3.0
 description: This module contains functions that calculate total score for given evaluation
 """
 
-import math
-
 
 def calculate_total_score_stroop(mistake_count: int) -> int:
-    total_score_stroop = max(1, min(5, 6 - int((mistake_count / 30) * 5)))
+    num_questions = 20
+    if mistake_count <= num_questions * 0.1:
+        total_score_stroop = 5
+    elif mistake_count <= num_questions * 0.2:
+        total_score_stroop = 4
+    elif mistake_count <= num_questions * 0.4:
+        total_score_stroop = 3
+    elif mistake_count <= num_questions * 0.6:
+        total_score_stroop = 2
+    else:
+        total_score_stroop = 1
     return total_score_stroop
+
 
 def calculate_total_score_trail_making(time: int, mistake_count: int) -> int:
     base_score = 6
     total_score_trail = max(1, min(5, int(base_score - (time / 300 + mistake_count / 30) * 2.5)))
     return total_score_trail
 
-def calculate_total_score_digit_substitution(mistake_count: int, correct_answers: int) -> int:
+
+def calculate_total_score_digit_substitution(mistake_count: int, correct_answers: int, total_time: int = 120) -> int:
     total_attempts = mistake_count + correct_answers
-    if total_attempts == 0:
+    if total_attempts == 0 or total_time == 0:
         return 1
     accuracy = correct_answers / total_attempts
-    total_score_digit = max(1, min(5, int(1 + 4 * accuracy)))
+    time_efficiency = correct_answers / total_time
+    total_score_digit = max(1, min(5, int(1 + 2 * accuracy + 2 * time_efficiency)))
     return total_score_digit
